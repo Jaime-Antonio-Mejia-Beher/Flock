a69169ab43dba3e23c52b622d82cc0a796ab0596
Kate Iliu <beviatori@gmail.com>
2024-05-29 13:10:03 -0600

    Added navbar and footer

---
+++ src/pages/nav/Footer.jsx
@@ -0,0 +1,22 @@
+
+const footerStyle = {
+    position: 'fixed',
+    bottom: 0,
+    left: 0,
+    width: '100%',
+    backgroundColor: '#f2f2f2', 
+    padding: '20px', 
+    textAlign: 'center',
+  };
+
+
+function Footer() {
+  return (
+    <div style={footerStyle}>
+        <p>© 2024 All Rights Reserved</p>
+    </div>
+  )
+}
+
+export default Footer
+
---
+++ src/pages/nav/Nav.jsx
@@ -0,0 +1,101 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+
+
+const menuContainerStyle = {
+  position: 'fixed',
+  top: 0,
+  left: 0,
+  width: '100%',
+  backgroundColor: '#FFFFFF',
+  padding: '10px',
+  display: 'flex',
+  justifyContent: 'space-between', // Positions logo and buttons at opposite ends
+  alignItems: 'center',
+};
+
+const logoStyle = {
+  width: '100px',
+  height: 'auto',
+};
+
+const buttonStyle = {
+  backgroundColor: '#EBF8FF',
+  marginRight: '15px'
+};
+
+export default function PositionedMenu() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const [isLoggedIn, setIsLoggedIn] = React.useState(false); // Track user's login state
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+    setIsLoggedIn(true);
+  };
+
+
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+  };
+
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+  };
+
+  return (
+    <div style={menuContainerStyle}>
+      <img src='/logo.svg' alt='logo' style={logoStyle} />
+      <div>
+        {isLoggedIn ? (
+          <>
+            <Button
+              id="demo-positioned-button"
+              aria-controls={open ? 'demo-positioned-menu' : undefined}
+              aria-haspopup="true"
+              aria-expanded={open ? 'true' : undefined}
+              onClick={handleClick}
+              style={buttonStyle}
+            >
+              My Account
+            </Button>
+            <Button
+              id="demo-positioned-button"
+              aria-controls={open ? 'demo-positioned-menu' : undefined}
+              aria-haspopup="true"
+              aria-expanded={open ? 'true' : undefined}
+              onClick={handleLogout}
+              style={buttonStyle}
+            >
+              Logout
+            </Button>
+          </>
+        ) : (
+            <>  
+          <Button
+            id="demo-positioned-button"
+            aria-controls={open ? 'demo-positioned-menu' : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? 'true' : undefined}
+            onClick={handleClick}
+            style={buttonStyle}
+          >
+            Sign In
+          </Button>
+
+        <Button
+          id="demo-positioned-button-signup"
+          aria-controls={open ? 'demo-positioned-menu-signup' : undefined}
+          aria-haspopup="true"
+          aria-expanded={open ? 'true' : undefined}
+          onClick={handleLogin}
+          style={{ ...buttonStyle, marginRight: '20px' }}
+        >
+          Sign Up
+        </Button>
+        </>
+        )}
+      </div>
+    </div>
+  );
+}
