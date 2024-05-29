fd3a4629598a9f252715c20d21a884162833aa21
Tan <tan10tvo@gmail.com>
2024-05-29 13:34:35 -0600

    changes to landing and explore

--- src/pages/landing/Landing.jsx
+++ src/pages/landing/Landing.jsx
@@ -1,18 +1,17 @@
 import { Typography } from "@mui/material";
-import Header from '../../components/Header';
-import ExploreSection from './componets/ExploreSection';
-import Footer from "../../components/Footer";
+import ExploreSection from "./componets/ExploreSection";
+
 
 const Landing = () => {
 
     return (
         <>
-             <Header />
+             
              <ExploreSection />
             <Typography variant='h3' color='primary'>Flock</Typography>
             <p>Twitter with a twist</p>
 
-            <Footer/>
+          
         </>
     )
 }
