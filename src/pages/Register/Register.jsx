e470788d67a9978092845566d176158159fd2138
Najiba-Haidari <nhaidari089@gmail.com>
2024-05-29 11:56:52 -0600

    confirmPassword added

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,14 +1,25 @@
 import { useState } from "react";
-
+import axios from "axios"
 
 export default function Register() {
     const [username, setUsername] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
 
-    const handleSubmit = (e) => {
+
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        console.log("Registered");
+        if (password === confirmPassword) {
+            try {
+                const response = await axios.post('/api/users', { username, email, password });
+                console.log(response.data); 
+            } catch (err) {
+                console.log(err); 
+            }
+        } else {
+            console.log("Passwords do not match");
+        }
     };
 
     return (
@@ -31,6 +42,13 @@ export default function Register() {
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
                 placeholder="Password"
+                required
+            />
+            <input
+                type="password"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+                placeholder="Confirm Password"
                 required
             />
             <button type="submit" >Register</button>
