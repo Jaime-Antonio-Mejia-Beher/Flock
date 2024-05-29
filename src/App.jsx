b3f455272eaefeb016e902d7c32783727513e405
chadcito <145295470+chadcito@users.noreply.github.com>
2024-05-29 10:26:35 -0600

    Added landing page and removed useState from app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-
+import Landing from './pages/landign/Landing'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Flock Project</h1>
+      <Landing />
     </>
   )
 }
