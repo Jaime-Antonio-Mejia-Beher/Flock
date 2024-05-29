4a515448f24a14d05605cfa73183eb4583a9192f
Najiba-Haidari <nhaidari089@gmail.com>
2024-05-29 14:39:11 -0600

    styled

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import axios from "axios"
+import { TextField, Button, Typography, Container } from '@mui/material';
+
 
 export default function Register() {
     const [username, setUsername] = useState("");
@@ -13,9 +15,9 @@ export default function Register() {
         if (password === confirmPassword) {
             try {
                 const response = await axios.post('/api/users', { username, email, password });
-                console.log(response.data); 
+                console.log(response.data);
             } catch (err) {
-                console.log(err); 
+                console.log(err);
             }
         } else {
             console.log("Passwords do not match");
@@ -23,37 +25,66 @@ export default function Register() {
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <input
+        <Container >
+        <form onSubmit={handleSubmit} style={{ boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)', padding: '30px', borderRadius: '8px', backgroundColor: 'white' }} >
+        <Typography variant="h4" className="exploreContent">Sign-up Form</Typography>
+            <TextField
+                helperText=""
+                id="demo-helper-text-aligned"
+                label="Username"
                 type="text"
                 value={username}
                 onChange={(e) => setUsername(e.target.value)}
                 required
+                fullWidth
+                margin="normal"
             />
-            <input
+            <br />
+            <TextField
+                helperText=""
+                id="demo-helper-text-aligned"
+                label="Email"
                 type="email"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
                 placeholder="Email"
+                fullWidth
                 required
+                margin="normal"
             />
-            <input
+            <br />
+
+            <TextField
+                helperText=""
+                id="demo-helper-text-aligned"
+                label="Password"
                 type="password"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
                 placeholder="Password"
                 required
+                fullWidth
+                margin="normal"
             />
-            <input
+            <br />
+
+            <TextField
+                helperText=""
+                id="demo-helper-text-aligned"
+                label="Confirm Password"
                 type="password"
                 value={confirmPassword}
                 onChange={(e) => setConfirmPassword(e.target.value)}
                 placeholder="Confirm Password"
                 required
+                fullWidth
+                margin="normal"
             />
-            <button type="submit" >Register</button>
-        </form>
+            <br />
 
+            <Button type="submit" variant="contained" color="primary" className="button" fullWidth >Submit</Button>
+        </form>
+</Container>
     )
 
 }
