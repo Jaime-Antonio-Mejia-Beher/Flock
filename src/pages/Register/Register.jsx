bd36597f87936c3a66afe1f45c81387eb82f89f1
Najiba-Haidari <nhaidari089@gmail.com>
2024-05-29 11:09:43 -0600

    Register page added

---
+++ src/pages/Register/Register.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+
+
+export default function Register() {
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("Registered");
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input
+                type="text"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                required
+            />
+            <input
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="Email"
+                required
+            />
+            <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Password"
+                required
+            />
+            <button type="submit" >Register</button>
+        </form>
+
+    )
+
+}
